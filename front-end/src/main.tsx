488adefbcb12b9be74c12962e3988230e776e0fd
Rosso2004 <51411152+Rosso2004@users.noreply.github.com>
2024-05-17 00:10:25 +0200

    Fix Reaload page nella release

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,15 +2,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import {GlobalStateProvider} from "./global/GlobalStateContext";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <GlobalStateProvider>
-      <BrowserRouter>
+        <HashRouter>
           <App />
-      </BrowserRouter>
+        </HashRouter>
     </GlobalStateProvider>
   // </React.StrictMode>,
 )
