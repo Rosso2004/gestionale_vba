6faf86880ce0d4436a521346044e91cfddbe29df
Rosso2004 <51411152+Rosso2004@users.noreply.github.com>
2024-06-28 16:04:23 +0200

    asd

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,13 +2,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom';
 import {GlobalStateProvider} from "./global/GlobalStateContext";
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <GlobalStateProvider>
-        <HashRouter>
+      <HashRouter>
           <App />
         </HashRouter>
     </GlobalStateProvider>
